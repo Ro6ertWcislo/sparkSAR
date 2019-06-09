30bef2f7da2df4340320c3d2f51f8f3ea65247cc
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2019-06-09 18:31:35 +0200

    Added demo of processing Cloud Optimized GeoTIFF from HDFS

--- src/main/scala/demo/Main.scala
+++ src/main/scala/demo/Main.scala
@@ -1,24 +1,35 @@
 package demo
-import geotrellis.raster.io.geotiff.reader.GeoTiffReader
-import geotrellis.raster.io.geotiff._
+
 import geotrellis.raster.Tile
+import geotrellis.spark._
 import geotrellis.spark.io._
-import geotrellis.spark.{LayerId, Metadata, SpatialKey, TileLayerMetadata}
-import geotrellis.spark.io.FilteringLayerReader
-import geotrellis.spark.io.file.FileLayerReader
-import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
+import geotrellis.spark.io.hadoop._
+import geotrellis.vector.ProjectedExtent
+import org.apache.spark.SparkContext
+
+import scala.reflect.io.Path
 
 object Main {
   def helloSentence = "Hello GeoTrellis"
 
   def main(args: Array[String]): Unit = {
     val conf = new org.apache.spark.SparkConf()
+      .set("spark.executor.memory", "4096m")
+      .set("spark.memory.fraction", "0.2")
     conf.setMaster("local[*]")
     implicit val sc:SparkContext = geotrellis.spark.util.SparkUtils.createSparkContext("sarDemo", conf)
 
-    val path: String = "/home/robert/Downloads/tif2/measurement/s1b-iw-grd-vv-20190605t075801-20190605t075818-016558-01f2c1-001.tiff"
-    val geoTiff: SinglebandGeoTiff = GeoTiffReader.readSingleband(path)
-    print(geoTiff)
+    // musi być COG tiff inaczej outofmemory
+    // gdal_translate ori.tiff out.tiff -co COMPRESS=LZW -co TILED=YES
+    val rdd = HadoopGeoTiffRDD.spatial("hdfs://localhost:9000/geotiffs/cogtest.tif")
+    println(rdd)
+
+    rdd.foreach(t => {
+      val (projectedExtent, tile) = t
+      println("tile " + projectedExtent.toString + " " + tile.rows)
+    })
+
+    println("\nMy watch has ended")
+
   }
 }
